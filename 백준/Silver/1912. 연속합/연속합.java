d8ce20ac0ce8f7a69858ed1e0d0e342132aa3925
Sibaru <leehakbin1217@gmail.com>
2025-10-25 23:03:55 +0900

    [Silver II] Title: 연속합, Time: 224 ms, Memory: 22948 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n]; // dp[i]: i번째 원소를 반드시 포함하는 최대 부분합
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        int max = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            // 이전 합 + 현재 값 vs 현재 값 하나로 시작
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
